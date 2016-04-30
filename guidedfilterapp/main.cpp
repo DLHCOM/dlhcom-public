6d3bd33fd625a535f3daf5aa81dcdb66f4df318d
lan ma <lanmahe@hotmail.com>
2016-04-29 21:09:38 -0700

    Initial Commit

---
+++ guidedfilterapp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  guidedfilterapp
+//
+//  Created by yingjian he on 4/29/16.
+//  Copyright © 2016 Sophia He. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
